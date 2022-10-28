0052269424d25cee81b9dbdacf609407ef2ea819
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-10-28 00:17:10 -0300

    feat: validates cep schema

--- back-end/src/routers/cepRouter.ts
+++ back-end/src/routers/cepRouter.ts
@@ -2,10 +2,11 @@ import {Router} from "express";
 
 import { findCep } from "../controllers/cepController.js";
 import { sanitizeInputs } from "../middlewares/sanitizeInputs.js";
-import { cepProperties } from "../schemas/cepSchema.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { cepProperties, cepSchema, cepErrorMessage } from "../schemas/cepSchema.js";
 
 const cep = Router();
 
-cep.get("/cep", sanitizeInputs(cepProperties), findCep);
+cep.get("/cep", sanitizeInputs(cepProperties), validateSchema(cepSchema, cepErrorMessage), findCep);
 
 export default cep;
--- back-end/src/schemas/cepSchema.ts
+++ back-end/src/schemas/cepSchema.ts
@@ -1,2 +1,10 @@
+import Joi from "joi";
+
 export const cepProperties = ["cep"];
+
+export const cepSchema = Joi.object({
+    cep: Joi.string().pattern(new RegExp('^[0-9]{8}$')).required()
+});
+
+export const cepErrorMessage = "Formato de CEP inválido. Por favor, insira apenas os números do CEP.";
 
