02d12e58f14dcf9214d31ef5799468cc6ce484c7
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-10-29 12:51:54 -0300

    chore: changes cep schema

--- back-end/src/factories/formatAddress.ts
+++ back-end/src/factories/formatAddress.ts
@@ -2,7 +2,7 @@ import { ApiAddress, FormatedAddress } from "../services/cepService.js";
 
 function formatAddress(address: ApiAddress){
     delete address.ok; delete address.statusText;
-    const formatedAddress: FormatedAddress = {...address, code: address.code.replace("-", "")}
+    const formatedAddress: FormatedAddress = {...address};
     return formatedAddress;
 }
 
--- back-end/src/schemas/cepSchema.ts
+++ back-end/src/schemas/cepSchema.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const cepSchema = Joi.object({
-    cep: Joi.string().pattern(new RegExp('^[0-9]{8}$')).required()
+    cep: Joi.string().pattern(new RegExp('^[0-9]{5}-[0-9]{3}$')).required()
 });
 
 export const cepErrorMessage = "Formato de CEP inválido. Por favor, confira o CEP e tente novamente.";
